a5655318d0aa988d72e8231f2f3604b8dd539792
carmenfu006 <guess_006@hotmail.com>
2021-04-07 13:58:13 +0800

    Change path method

--- config/application.rb
+++ config/application.rb
@@ -5,11 +5,15 @@ require 'sinatra/reloader'
 require 'byebug'
 require 'sinatra/activerecord'
 require './config/environments' #database configuration
-require './models/player'
-require './models/result'
+# require './models/player'
+# require './models/result'
 
 require File.expand_path(File.join('helpers', 'application_helper'))
 require File.expand_path(File.join('controllers', 'application_controller'))
 
 Dir[File.join('helpers', '**/*._helper.rb')].each { |file| require File.expand_path(file) }
 Dir[File.join('controllers', '**/*_controller.rb')].each { |file| require File.expand_path(file) }
+
+Dir.glob("./models/*.rb").sort.each do |file|
+  require file
+end
